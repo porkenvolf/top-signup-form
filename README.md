a0e6f3814bc983625268042bde902f0d234361cf
Porkenvolf <89871231+porkenvolf@users.noreply.github.com>
2023-06-03 21:36:03 -0300

    Misc layout changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # top-signup-form
+
+to do:
+- button hover
+- restyle button
+- separate labels from inputs
--- js/validation.js
+++ js/validation.js
@@ -2,7 +2,7 @@ const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 confirmPassword.addEventListener("input", (event) => {
     if (confirmPassword.value !== password.value) {
-        confirmPassword.setCustomValidity("dsa");
+        confirmPassword.setCustomValidity("The passwords do not match");
     } else {
         confirmPassword.setCustomValidity("");
     }
